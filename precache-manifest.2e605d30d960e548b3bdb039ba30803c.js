492192c8fa182edbd09aff9eb2fb50c071ad7f8b
casperfibaek <casperfibaek@gmail.com>
2019-11-18 18:25:34 +0200

    react tut

---
+++ precache-manifest.2e605d30d960e548b3bdb039ba30803c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9e612c22e2e1c44c6d7255214bb034",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caa950da8384538e52db",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b82c23b5b06e24df306a",
+    "url": "/static/js/2.79a8cecc.chunk.js"
+  },
+  {
+    "revision": "caa950da8384538e52db",
+    "url": "/static/js/main.13af188b.chunk.js"
+  },
+  {
+    "revision": "dc7ed9572a2c778fbd82",
+    "url": "/static/js/runtime-main.4d719469.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
