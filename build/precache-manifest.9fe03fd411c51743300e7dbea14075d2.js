5e9b1fa51b874168832bac555c92574fb7bdee2f
casperfibaek <casperfibaek@gmail.com>
2019-11-18 18:39:21 +0200

    gitignore remove build

---
+++ build/precache-manifest.9fe03fd411c51743300e7dbea14075d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1764407876dc4a95aab914a0ad00eb1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "693f051440537e315dda",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b82c23b5b06e24df306a",
+    "url": "/static/js/2.79a8cecc.chunk.js"
+  },
+  {
+    "revision": "693f051440537e315dda",
+    "url": "/static/js/main.52f9e68d.chunk.js"
+  },
+  {
+    "revision": "dc7ed9572a2c778fbd82",
+    "url": "/static/js/runtime-main.4d719469.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
